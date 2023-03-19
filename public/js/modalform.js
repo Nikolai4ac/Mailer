51ba51e7c8b6da583c763bffd116d6286b7708a1
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2023-03-19 10:20:02 +0200

    successfully sending the body content as JSON to modalinfo.js

--- public/js/modalform.js
+++ public/js/modalform.js
@@ -6,18 +6,22 @@ function execute() {
     btnOk.onclick = function (event) {
         window.alert("SUCCESS!");
         event.preventDefault();
-        // let xhr = new XMLHttpRequest();
-        // xhr.open('POST', '/send-email-subject');
-        // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        // xhr.onload = function() {
-        //   if (xhr.status === 200) {
-        //     window.location.href = '/receiver-list';
-        //   }
-        //   else {
-        //     window.alert('Error: ' + xhr.statusText);
-        //   }
-        // };
-        // xhr.send(new FormData(document.getElementById('modalForm')));
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', '/send-email-data');
+        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+        xhr.onload = function() {
+          if (xhr.status === 200) {
+            alert('code 200 ok')
+          }
+          else {
+            window.alert('Error: ' + xhr.statusText);
+          }
+        };
+        let modalData = new FormData(document.getElementById('modalForm'));
+        let sendModal = Object.fromEntries(modalData);
+        console.log(sendModal);
+        console.log(JSON.stringify(sendModal));
+        xhr.send(JSON.stringify(sendModal));
     };
     span.onclick = function () {
       modal.style.display = "none";
