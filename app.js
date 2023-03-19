49d90080b7d6031e370736b5328b8cd4d0ed24fc
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2023-03-19 12:34:39 +0200

    fixed router3 import

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const Index = require('.//routes/Index')
-const path = require('path')
-const { router1 } = require('./routes/MySql')
-const router2 = require('./routes/receivers')
-const router3 = require('./routes/prepare')
+const Index = require('.//routes/Index');
+const path = require('path');
+const { router1 } = require('./routes/MySql');
+const router2 = require('./routes/receivers');
+const {router3} = require('./routes/prepare');
+const router4 = require('./routes/modalinfo');
+const bodyParser = require('body-parser');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}))
 app.use('/', Index);
 app.use('/', router1);
 app.use('/', router2);
-app.use('/', router3)
+app.use('/', router3);
+app.use('/', router4);
+app.use(bodyParser.json());
 app.get('/', (req,res) => {
     res.redirect('/receiver-list');
 })
 app.use("/public/", express.static('./public/'))
-app.use(express.json());
 app.listen(port, (error) => { 
     if (error) { 
         throw error;
