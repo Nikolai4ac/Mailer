784240b0da6c86bd5a2e3f8d56456e010aaef524
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2023-03-19 20:35:41 +0200

    changed the way how the data is deleted

--- public/js/deletedata.js
+++ public/js/deletedata.js
@@ -1,4 +1,15 @@
 function deleteUser(event) {
+    const tbody = document.getElementById('tblrow')
+    const selectedRows = [...tbody.children].filter(tr => tr.children[0].firstChild.checked)
+    const dataToDelete = [];
+    for (let i = 0; i < selectedRows.length; i++) {
+      const first_name = selectedRows[i].children[1].textContent;
+      const last_name = selectedRows[i].children[2].textContent;
+      const email = selectedRows[i].children[3].textContent;
+      const objData = {first_name: first_name, last_name: last_name, email: email}
+      dataToDelete.push(objData);
+    }
+    const stringifiedDataForDump = JSON.stringify(dataToDelete);
     event.preventDefault();
     let xhr = new XMLHttpRequest();
     xhr.open('DELETE', '/delete-user-data')
@@ -12,8 +23,5 @@ function deleteUser(event) {
             window.alert('Error: ' + xhr.statusText);
         }
     };
-    let formDestructionData = new FormData(document.getElementById('sender'));
-    let sentDestructionData = Object.fromEntries(formDestructionData);
-    console.log(sentDestructionData);
-    xhr.send(JSON.stringify(sentDestructionData));
+    xhr.send(stringifiedDataForDump);
 }
