28d5986787166897a9f5c097f2d794e24342c321
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2023-03-19 20:36:57 +0200

    removed delete request

--- routes/Index.js
+++ routes/Index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:false}))
 const { insertInTable } = require('../routes/MySql');
-const { deleteFromTable } = require('../routes/MySql');
 router.post('/create-list', (req,res) => {
     const fname = req.body.fname;
     const lname = req.body.lname;
@@ -12,11 +11,5 @@ router.post('/create-list', (req,res) => {
     insertInTable([fname, lname, email])   
     res.redirect('/receiver-list');
 })
-router.delete('/delete-user-data', (req,res) => {
-    const fname = req.body.fname;
-    const lname = req.body.lname;
-    const email = req.body.email;
-    deleteFromTable([fname, lname, email])
-    res.end();
-})
+
 module.exports = router;
