052afcb1eaa75944872ba5e2b63901df4e293265
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2023-03-19 10:17:06 +0200

    simplified string creation for the email receivers

--- public/js/checkAll.js
+++ public/js/checkAll.js
@@ -1,22 +1,25 @@
 function logInfo() {
     const tbody = document.getElementById('tblrow')
     const selectedRows = [...tbody.children].filter(tr => tr.children[0].firstChild.checked)
-    const data = selectedRows.map((row) => ({
-      first_name: row.children[1].textContent,
-      last_name: row.children[2].textContent,
-      email: row.children[3].textContent
-    }))
-    const sendString = JSON.stringify(data);
+    const testData = [];
+    for (let i = 0; i < selectedRows.length; i++) {
+      const first_name = selectedRows[i].children[1].textContent;
+      const last_name = selectedRows[i].children[2].textContent;
+      const email = selectedRows[i].children[3].textContent;
+      const objData = {email: email}
+      testData.push(objData);
+    }
+    const stringifiedData = JSON.stringify(testData);
     let xhr = new XMLHttpRequest();
     xhr.open('POST', '/send-email');
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.onload = function() {
       if (xhr.status === 200) {
-        alert(sendString);
+        alert(xhr.status)
       }
       else {
         window.alert('Error: ' + xhr.statusText);
       }
     };
-    xhr.send(sendString)
+    xhr.send(stringifiedData)
 }
