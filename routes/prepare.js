b90d1df4385b1c52934a14aa55029de877c8f8df
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2023-03-19 20:38:12 +0200

    added http handle for DELETE request

--- routes/prepare.js
+++ routes/prepare.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
 const router3 = express.Router();
+const { deleteFromTable } = require('../routes/MySql');
 router3.use(bodyParser.json());
 let receiversString;
 router3.post('/send-email', (req, res) => {
@@ -11,6 +11,16 @@ router3.post('/send-email', (req, res) => {
         receivers += data[i].email += ', ';
     }
     receiversString = receivers.slice(0, -2);
+    res.end();
+})
+router3.delete('/delete-user-data', (req, res) => {
+    const data = req.body;
+    for (let el of data) {
+        const firstName = el.first_name;
+        const lastName = el.last_name;
+        const email = el.email;
+        deleteFromTable([firstName, lastName, email])
+    }
     res.end();
 })
 module.exports = {router3, getReceiversString: function () {
